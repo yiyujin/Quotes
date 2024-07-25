02f811aa4191e4d8486659587ecaf65540db5697
yiyujin <yujin94@gmail.com>
2024-07-25 09:57:34 +0900

    added a page List that does getQuotesList in a table format

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,10 +3,11 @@
 import { sql } from '@vercel/postgres';
 import { User, Book, Quote } from './definitions';
 
-export async function getUser(email: string) {
+export async function getUser( email: string ) {
   try {
     const user = await sql<User>`
-    SELECT * FROM users WHERE email = ${email}
+      SELECT * FROM users
+      WHERE email = ${ email };
     `;
 
     return user.rows[0] as User;
@@ -22,7 +23,7 @@ export async function getQuotes( book_id : string ) {
         const quotes = await sql<Quote>`
           SELECT * FROM quotes
           WHERE book_id = ${ book_id }
-          AND deleted = 'N'
+          AND deleted = 'N';
         `;
 
         const data = quotes.rows;
@@ -37,10 +38,48 @@ export async function getQuotes( book_id : string ) {
 
 export async function getBooks() {
   try {
-    const books = await sql<Book>`SELECT * FROM books ORDER BY created_date DESC`;
+    const books = await sql<Book>`
+      SELECT * FROM books
+      ORDER BY created_date DESC;
+    `;
+
     const data = books.rows;
     
     return data;
+  } catch (error) {
+      console.error('Failed to fetch quotes:', error);
+      throw new Error('Failed to fetch quotes.');
+  }
+}
+
+export async function getQuotesList( book_id : string ) {
+  try {
+      const quotes = await sql<Quote>`
+        SELECT * FROM quotes
+        ORDER BY book_id;
+      `;
+
+      const quotesList = quotes.rows;
+      const quotesListCount = quotes.rowCount;
+
+      return { quotesList, quotesListCount };
+  } catch (error) {
+      console.error('Failed to fetch quotes:', error);
+      throw new Error('Failed to fetch quotes.');
+  }
+}
+
+export async function getBooksList() {
+  try {
+    const books = await sql<Book>`
+      SELECT * FROM books
+      ORDER BY created_date DESC;
+    `;
+
+    const booksList = books.rows;
+    const booksListCount = books.rowCount;
+    
+    return { booksList, booksListCount };
   } catch (error) {
       console.error('Failed to fetch quotes:', error);
       throw new Error('Failed to fetch quotes.');
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,7 +1,48 @@
+import { getQuotesList, getBooksList } from "../lib/data"
+
 export default async function List(){
+
+    const { quotesList, quotesListCount } = await getQuotesList();
+    // const { booksList, booksListCount } = await getBookList();
+
+
     return(
         <>
-            <h1>List Page</h1>
+            <div className = "flex flex-col h-full w-full">
+
+                <h1>Quotes List</h1>
+
+                <div className = "">
+                    <h1>rowCount: { quotesListCount }</h1>
+                </div>
+
+                <div className = "flex h-full overflow-y-auto">
+                    <table>
+                        <tr>
+                            <th>book_id</th>
+                            <th>id</th>
+                            <th>page_number</th>
+                            <th>content</th>
+                        </tr>
+                        
+                        { quotesList.map(( quote ) => (
+                            <tr key = { quote.id } className = "">
+                                <td>{ quote.book_id }</td>
+                                <td>{ quote.id }</td>
+                                <td>{ quote.page_number }</td>
+                                <td>{ quote.content }</td>
+                                
+                                {/* <form action = { deleteQuote } method = "DELETE">
+                                    <input type = "hidden" name = "id" value = { quote.id } />
+                                    <button type = "submit" className = "text-xs rounded-md border p-2 hover:bg-gray-100">DELETE</button>
+                                </form> */}
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            
+            </div>
+
         </>
     )
 };
