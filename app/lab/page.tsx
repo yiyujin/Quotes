1e3744d4f53206bbb97882edecdc0568afbc543d
yiyujin <yujin94@gmail.com>
2024-08-05 21:53:24 +0900

    revised matter.js to receive quotesList as data

--- app/lab/page.tsx
+++ app/lab/page.tsx
@@ -3,17 +3,13 @@ import { MatterTest } from '../ui/lab/matterjs-test';
 import { getQuotesList, getQuotesCount } from '../lib/data';
 
 export default async function MatterPage(){
-    const  { quotesList, quotesListCount } = await getQuotesList();
-
-    const { data2 } = await getQuotesCount();
-
-    console.log(data2);
+    const  { quotesList } = await getQuotesList();
 
     return (
         <>
             <div className = "">
                 {/* <MatterTest quotesListCount = { quotesListCount }/> */}
-                <MatterTest quotesListCount = { quotesList }/>
+                <MatterTest quotesList = { quotesList }/>
             </div>
         </>
     );
--- app/ui/lab/matterjs-test.tsx
+++ app/ui/lab/matterjs-test.tsx
@@ -5,13 +5,15 @@ import Matter from "matter-js";
 
 // CREATE CANVAS VARIABLES
 const r = 10;
-const wallWidth = 4;
+const wallWidth = 10;
 const width = window.innerWidth - 260 - 32;
 const height = window.innerHeight - wallWidth / 2;
 const colors = ['#AF50FF', '#00BEFF', '#FF685F', '#F7C839','#FF3E0D', '#FD9540', '#00D37E'];
 let c;
 
-export function MatterTest( { quotesListCount } : { quotesListCount : object }, { data } : { data : object } ) {
+let balls = [];
+
+export function MatterTest( { quotesList} : { quotesList : object}) {
     const containerRef = useRef(null);
     const canvasRef = useRef(null);
     const engineRef = useRef(null);
@@ -52,21 +54,21 @@ export function MatterTest( { quotesListCount } : { quotesListCount : object }, 
         const ground = Bodies.rectangle(width / 2, height, width, wallWidth, {
             isStatic: true,
             render: {
-                fillStyle: 'rgba(0, 0, 0, 0)',
+                fillStyle: 'rgba(0, 0, 0, 1)',
             }
         });
 
         const leftWall = Bodies.rectangle(0, height / 2, wallWidth, height, {
             isStatic: true,
             render: {
-                fillStyle: 'rgba(0, 0, 0, 0)',
+                fillStyle: 'rgba(0, 0, 0, 1)',
             }
         });
 
         const rightWall = Bodies.rectangle(width, height / 2, wallWidth, height, {
             isStatic: true,
             render: {
-                fillStyle: 'rgba(0, 0, 0, 0)',
+                fillStyle: 'rgba(0, 0, 0, 1)',
             }
         });
 
@@ -74,25 +76,45 @@ export function MatterTest( { quotesListCount } : { quotesListCount : object }, 
         World.add(engine.world, [ground, leftWall, rightWall]);
 
         // ADD BALLS TO THE WORLD BASED ON quoteCount
-        for (let i = 0; i < quotesListCount.length; i++) {
+        // for (let i = 0; i < quotesListCount.length; i++) {
 
-            //ASSIGN COLORS BY COUNT GROUP BY BOOK_ID
+        //     //ASSIGN COLORS BY COUNT GROUP BY BOOK_ID
 
-            if(i <= 50){
-                c = colors[0];
-            } else {
-                c = colors[1];
-            }
+        //     if(i <= 50){
+        //         c = colors[0];
+        //     } else {
+        //         c = colors[1];
+        //     }
 
-            const ball = Bodies.circle(Math.random() * width, Math.random() * height, r, {
-                restitution: 0.9,
-                render: {
-                    fillStyle: c,
-                },
-            });
-            World.add(engine.world, ball);
-        }
+        //     const ball = Bodies.circle(Math.random() * width, Math.random() * height, r, {
+        //         restitution: 0.9,
+        //         render: {
+        //             fillStyle: c,
+        //         },
+        //     });
+        //     World.add(engine.world, ball);
+        // }
 
+        // { quotesList.map(( quote ) => (
+
+        //     let ball = Bodies.circle(Math.random() * width, Math.random() * height, r, {
+        //         restitution: 0.9,
+        //         render: {
+        //             fillStyle: c,
+        //         },
+        //     });
+
+        //     balls.push()
+
+        //     <tr key = { quote.id } className = "">
+        //         <td>{ quote.book_id }</td>
+        //         <td>{ quote.id }</td>
+        //         <td>{ quote.page_number }</td>
+        //         <td>{ quote.content }</td>
+        //     </tr>
+        // ))}
+
+        
         // RUN THE ENGINE AND RENDER/CANVAS
         Runner.run(runner, engine);
         Render.run(render);
