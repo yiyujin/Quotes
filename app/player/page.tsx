73467654d465fda48c5c65953f990574f4f918ba
yiyujin <yujin94@gmail.com>
2025-01-16 03:00:21 -0500

    add random button to player quote

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -1,13 +1,42 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import { getRandomQuote } from "../lib/data";
 
-export default async function Player(){
-    const quote = await getRandomQuote();
+interface Quote {
+  data: string;
+}
 
-    return(
+export default function Player() {
+    const [quote, setQuote] = useState<Quote | null>(null);
+
+    useEffect(() => {
+        fetchNewQuote();
+    }, []);
+
+    const fetchNewQuote = async () => {
+        try {
+            const newQuote = await getRandomQuote();
+            // Handle the case where newQuote might be undefined
+            if (newQuote) {
+                setQuote(newQuote);
+            } else {
+                setQuote({ data: 'Failed to load quote' });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
         <div className = "page">
-            <div className = "playerQuote">
-                <h1>{ quote?.data }</h1>
+            <div style = { { display : "flex", flexDirection : "column", height : "100%" } }>
+                <div className = "playerQuote">
+                    <h1>{ quote?.data }</h1>
+                </div>
+
+                <button className = "Button" onClick = { fetchNewQuote }>Random</button>
             </div>
         </div>
-    )
-};
+    );
+}
